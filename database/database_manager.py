82b863677e6535daac207b5f8b10bb31bbae3eea
gbaka <geo.m2019@mail.ru>
2023-09-16 05:31:14 +0300

    refactoring

--- database/database_manager.py
+++ database/database_manager.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
-from database.models import Record, Chat, Base
+from database.models import Record, Chat, Text, Base
 from errors.custom_errors import CommandArgumentError
 import config
 import datetime
@@ -14,73 +14,78 @@ class DatabaseManager:
         self.metadata.create_all(self.engine)          # создание таблиц в соответствии с метаданными
         self.Session = sessionmaker(bind=self.engine)  # создание фабрики сессий (через нее создаются сессии)
 
-    def create_history_record(self, user_id, user_name, command, command_arguments, status):
-        session = self.Session()
-        new_record = Record(
-            user_name=user_name,
-            user_id=user_id,
-            command=command,
-            command_arguments=command_arguments,
-            date=datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
-            status=status
-        )
-        session.add(new_record)   # AIUI: session.add определяет, в какую таблицу нужно добавить запись,
-                                  # на основе класса объекта, переданного в качестве аргумента
-        session.commit()  
-        self._trim_history(session)  # удаялем старые записи из истории команд
-        session.close()
-
-    # def get_record(self, record_id):
-    #     session = self.Session()
-    #     record = session.query(Record).filter_by(id=record_id).first()
-    #     session.close()
-    #     return record
-
-    # def update_record(self, record_id, new_command):
-    #     session = self.Session()
-    #     record = session.query(Record).filter_by(id=record_id).first()
-    #     if record:
-    #         record.command = new_command
-    #         session.commit()
-    #     session.close()
-
-    def get_history_page(self, page):
-        if page < 1:
-            raise CommandArgumentError("Номер страницы не может быть меньше единицы.")
-        session = self.Session()
-        records = session.query(Record
-            ).order_by(desc(Record.record_id)
-            ).offset((page-1)*config.HISTORY_PAGE_CAPACITY
-            ).limit(config.HISTORY_PAGE_CAPACITY)
-        session.close()
-        return records
+        self.history = DatabaseManager._History(self.Session)           # клас _History группирует методы для работы с историей комманд
 
 
-    def get_all_records(self):
-        session = self.Session()
-        records = session.query(Record).order_by(desc(Record.record_id)).all()
-        session.close()
-        return records
+    class _History:
+        def __init__(self, Session) -> None:
+            self.Session = Session
 
+        def create_record(self, user_id, user_name, command, command_arguments, status):
+            session = self.Session()
+            new_record = Record(
+                user_name=user_name,
+                user_id=user_id,
+                command=command,
+                command_arguments=command_arguments,
+                date=datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
+                status=status
+            )
+            session.add(new_record)   # AIUI: session.add определяет, в какую таблицу нужно добавить запись,
+                                    # на основе класса объекта, переданного в качестве аргумента
+            session.commit()  
+            self._trim(session)  # удаялем старые записи из истории команд
+            session.close()
 
-    def _trim_history(self, session):
-        # session = self.Session()
-        record_count = session.query(Record).count()
-        if record_count > config.HISTORY_CAPACITY:
-            records_to_delete = record_count - config.HISTORY_CAPACITY
-            record_ids_to_delete = session.query(Record.record_id).order_by(Record.record_id).limit(records_to_delete)
-            session.query(Record).filter(Record.record_id.in_(record_ids_to_delete)).delete()
-        session.commit()
-        # session.close()
+        def get_page(self, page):
+            if page < 1:
+                raise CommandArgumentError("Номер страницы не может быть меньше единицы.")
+            session = self.Session()
+            records = session.query(Record
+                ).order_by(desc(Record.record_id)
+                ).offset((page-1)*config.HISTORY_PAGE_CAPACITY
+                ).limit(config.HISTORY_PAGE_CAPACITY)
+            session.close()
+            return records
 
-
-    def delete_record(self, record_id):
-        session = self.Session()
-        record = session.query(Record).filter_by(id=record_id).first()
-        if record:
-            session.delete(record)
+        def _trim(self, session):
+            record_count = session.query(Record).count()
+            if record_count > config.HISTORY_CAPACITY:
+                records_to_delete = record_count - config.HISTORY_CAPACITY
+                record_ids_to_delete = session.query(Record.record_id).order_by(Record.record_id).limit(records_to_delete)
+                session.query(Record).filter(Record.record_id.in_(record_ids_to_delete)).delete()
             session.commit()
-        session.close()
+
+        def get_record_count(self):
+            session = self.Session()
+            records_number = session.query(Record).count()
+            session.close()
+            return records_number
+
+        def clear(self):
+            session = self.Session()
+            records_number = session.query(Record).delete()
+            session.commit()
+            session.close()
+
+        def is_empty(self):
+            return not bool(self.get_record_count())
+
+
+
+
+
+    
+
+
+
+
+
+# class Texts():
+#     def __init__(self, Session) -> None:
+#         self.Session = Session
+
+
 
 
 DATABASE_MANAGER = DatabaseManager(config.CONNECT_STRING)
