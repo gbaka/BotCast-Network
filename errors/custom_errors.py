1f0f33b6e4699b877c6cd12d39b4f474699168ee
gbaka <geo.m2019@mail.ru>
2023-09-17 09:42:33 +0300

    refactoring error's classes

--- errors/custom_errors.py
+++ errors/custom_errors.py
@@ -1,16 +1,34 @@
-class CommandArgumentError(Exception):
+class BaseCommandError(Exception):
     """
-    Возникает в случае неправильных аргументов, которые
-    передал пользователь.
+    Базовый класс для пользовательских исключений, связанных с командами.
     """
-    def __init__(self, message):
+    def __init__(self, message="", title=""):
         super().__init__(message)
+        self.title = title
 
-        
-class CommandExecutionError(Exception):
+    def set_title(self, title: str):
+        self.title = title
+
+    def get_title(self):
+        return self.title
+
+    def __str__(self):
+        return f"{self.title}\n\n{self.args[0]}"
+
+
+class CommandArgumentError(BaseCommandError):
+    """
+    Возникает в случае неправильных аргументов, которые передал пользователь.
+    """
+    def __init__(self, message=""):
+        super().__init__(message, "⚠️ **Проверьте правильность преданных аргументов.**")
+
+
+class CommandExecutionError(BaseCommandError):
     """
     Возникает в случае если формально аргументы пользователя верны, но
     Telegram API не позволил выполнить команду.
     """
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, message=""):
+        super().__init__(message, "⚠️ **Ошибка при выполнении команды.**")
+
