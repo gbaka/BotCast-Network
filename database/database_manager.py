778b061e1b27542d6ca83258cb594bf5d0796c05
gbaka <geo.m2019@mail.ru>
2023-09-26 01:13:21 +0300

    added helper Messages and Notes classes

--- database/database_manager.py
+++ database/database_manager.py
@@ -1,17 +1,18 @@
-from sqlalchemy import create_engine, desc
+from sqlalchemy import create_engine, desc, func
 from sqlalchemy.orm import sessionmaker
-from database.models import Record, Chat, Text, Base
+from database.models import Record, Chat, Text, DelayedMessage, Note, Base
 from errors.custom_errors import CommandArgumentError
 from utils import helpers
 import config
 import datetime
+from pyrogram import types
 
-# TODO: вместо ручного открытия и закрытия сессий использовать with 
+# TODO: вместо ручного открытия и закрытия сессий использовать with !!!
 
 
 class DatabaseManager:
     def __init__(self, database_url):
-        self.engine = create_engine(database_url, echo=True)  # создается интерфейс для доступа к БД
+        self.engine = create_engine(database_url, echo=False)  # создается интерфейс для доступа к БД
         self.metadata = Base.metadata                         # метаданные о структуре таблиц
         self.metadata.create_all(self.engine)          # создание таблиц в соответствии с метаданными
         self.Session = sessionmaker(bind=self.engine)  # создание фабрики сессий (через нее создаются сессии)
@@ -19,6 +20,8 @@ class DatabaseManager:
         self.history = DatabaseManager._History(self.Session)           # клас _History группирует методы для работы с историей комманд
         self.texts = DatabaseManager._Texts(self.Session)
         self.chats = DatabaseManager._Chats(self.Session)
+        self.messages = DatabaseManager._Messages(self.Session)
+        self.notes = DatabaseManager._Notes(self.Session)
 
 
     class _History:
@@ -87,7 +90,7 @@ class DatabaseManager:
             session.commit()  
             session.close()
 
-        def get_text(self, text_id):
+        def get_text(self, text_id) -> Text:
             session = self.Session()
             query = session.query(Text).filter_by(text_id=text_id)
             if query.count() == 0:
@@ -95,6 +98,17 @@ class DatabaseManager:
             session.close()
             return query.one()
         
+        def get_texts(self) -> list[Text]:
+            with self.Session() as session:
+                records = session.query(Text).all()
+            return records
+        
+        def get_text_ids(self) -> list[int]:
+            with self.Session() as session:
+                records = session.query(Text).all()
+            text_ids = [text_id for record_tuple in records for text_id in record_tuple]
+            return text_ids
+            
         def get_record_count(self):
             session = self.Session()
             records_number = session.query(Text).count()
@@ -111,6 +125,12 @@ class DatabaseManager:
                 ).limit(config.TEXTS_PAGE_CAPACITY)
             session.close()
             return records
+        
+        def has_text(self, text_id: int):
+            session = self.Session()
+            query = session.query(Text).filter_by(text_id=text_id)
+            session.close()
+            return query.count() != 0
 
         def delete(self, text_id: int):
             session = self.Session()
@@ -147,6 +167,13 @@ class DatabaseManager:
             session.commit()  
             session.close()
 
+        def get_name(self, chat_id) -> str:
+            with self.Session() as session:
+                query = session.query(Chat.name).filter_by(chat_id=chat_id)
+                if query.count() == 0:
+                    raise CommandArgumentError("В базе нет чата с указанным ID.")
+            return query.one()[0]
+
         def delete(self, chat_id: int):
             session = self.Session()
             query = session.query(Chat).filter_by(chat_id=chat_id)
@@ -166,6 +193,14 @@ class DatabaseManager:
                 ).limit(config.CHATS_PAGE_CAPACITY)
             session.close()
             return records
+        
+        def get_chat(self, chat_id: int) -> Chat: 
+            session = self.Session()
+            query = session.query(Chat).filter_by(chat_id=chat_id)
+            if query.count() == 0:
+                raise CommandArgumentError("В базе нет чата с указанным ID.")
+            session.close()
+            return query.one() 
         
         def has_chat(self, chat_id: int):
             session = self.Session()
@@ -205,23 +240,149 @@ class DatabaseManager:
             session.commit()
             session.close()
 
+    class _Messages:
+        def __init__(self, Session: sessionmaker) -> None:
+            self.Session =  Session
+
+        def add(self, message_id, target_chat_id, schedule_date):
+            with self.Session() as session:
+                self._refresh(session)
+                delayed_message = DelayedMessage(
+                    message_id=message_id,
+                    target_chat_id=target_chat_id, 
+                    scheduled_date=schedule_date
+                )
+                session.add(delayed_message)                              
+                session.commit()  
+
+        def add_by_telegram_message_list(self, telegram_message_list : types.Message):
+            with self.Session() as session:
+                self._refresh(session)
+                for message in telegram_message_list:
+                    delayed_message = DelayedMessage(
+                        message_id=message.id, 
+                        target_chat_id=message.chat.id, 
+                        schedule_date=message.date,
+ 
+                    )
+                    session.add(delayed_message)
+                session.commit()
+
+        def get_message_count_by_chat_id(self, chat_id: int) -> int:
+            with self.Session() as session: 
+                self._refresh(session)
+                records = session.query(DelayedMessage).filter_by(target_chat_id=chat_id)
+            return records.count()
         
+        # ??? переписать эту хуйню
+        def get_messages_info_by_chats(self) -> list[tuple]:
+            with self.Session() as session: 
+                self._refresh(session)
+                records = session.query(DelayedMessage.target_chat_id, func.count(DelayedMessage.message_id), func.max(DelayedMessage.schedule_date)
+                                        ).group_by(DelayedMessage.target_chat_id
+                                        ).order_by(func.count().desc()).all()
+            return records
+        
+        def get_message_ids_by_chat_id(self, chat_id : int) -> list[tuple]:
+            with self.Session() as session:
+                self._refresh(session)
+                records = session.query(DelayedMessage.message_id).filter_by(target_chat_id=chat_id).all()
+            message_ids = [message_id for record_tuple in records for message_id in record_tuple]
+            return message_ids
+            
+
+        def delete_messages_by_chat_id(self) -> None:
+            pass
+
+        def delete_messages_by_ids(self, message_ids : list[int]): 
+            with self.Session() as session:
+                session.query(DelayedMessage).filter(DelayedMessage.message_id.in_(message_ids)).delete()
+                session.commit()
+        
+        def _refresh(self, session):
+            """ Удаляет неактуальные сообщения."""
+            current_date = datetime.datetime.now()
+            
+            session.query(DelayedMessage).filter(DelayedMessage.schedule_date < current_date).delete()
+            session.commit()
+
+    class _Notes:
+        def __init__(self, Session : sessionmaker) -> None:
+            self.Session = Session
+            
+        def add(self, note : str):
+            with self.Session() as session:
+                note = Note(note=note)
+                session.add(note)
+                session.commit()
+
+        def clear(self):
+            with self.Session() as session:
+                session.query(Note).delete()
+                session.commit()
+
+        def delete(self, note_id : int):
+            with self.Session() as session:
+                query = session.query(Note).filter_by(note_id=note_id)
+                if query.count() == 0:
+                    raise CommandArgumentError("В базе нет заметки с указанным ID.")
+                query.delete()
+                session.commit()
+
+        def get_page(self, page: int) -> list[Note]:
+            if page < 1:
+                raise CommandArgumentError("Номер страницы не может быть меньше единицы.")
+            with self.Session() as session:
+                records = session.query(Note
+                    ).order_by(desc(Note.note_id)
+                    ).offset((page-1)*config.NOTES_PAGE_CAPACITY
+                    ).limit(config.NOTES_PAGE_CAPACITY)
+                return records
+
+        # def get_note(self, note_id : int):
+        #     with self.Session() as session:
+        #         query = session.query(Note).filter_by(note_id=note_id)
+        #         if query.count() == 0:
+        #             raise CommandArgumentError("В базе нет заметки с указанным ID.")
 
 
+        #  def add(self, text: str):
+        #     session = self.Session()
+        #     new_record = Text(text=text)
+        #     session.add(new_record)   # AIUI: session.add определяет, в какую таблицу нужно добавить запись,                            
+        #     session.commit()  
+        #     session.close()
 
+        # def get_text(self, text_id) -> Text:
+        #     session = self.Session()
+        #     query = session.query(Text).filter_by(text_id=text_id)
+        #     if query.count() == 0:
+        #         raise CommandArgumentError("В базе нет текста с указанным ID.")
+        #     session.close()
+        #     return query.one()
+        
+        # def get_texts(self) -> list[Text]:
+        #     with self.Session() as session:
+        #         records = session.query(Text).all()
+        #     return records
 
+        def set_description(self, note_id, description):
+            with self.Session() as session:
+                query = session.query(Note).filter_by(note_id=note_id)
+                if query.count() == 0:
+                    raise CommandArgumentError("В базе нет заметки с указанным ID.")
+                note = query.one()
+                note.description = description
+                session.commit()
 
+        def get_notes_count(self):
+            with self.Session() as session:
+                query = session.query(Note)
+            return query.count()  
 
-    
-
-
-
-
-
-# class Texts():
-#     def __init__(self, Session) -> None:
-#         self.Session = Session
-
+        def is_empty(self):
+            return bool(self.get_notes_count() == 0)  
+            
 
 
 
